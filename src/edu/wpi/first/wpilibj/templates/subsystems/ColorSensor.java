94799305c20768f556990ed40aae6ad74d75657a
Jessie Adkins <zaheelee@gmail.com>
2013-03-02 15:17:48 -0500

    added the color sensor subsystem

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/ColorSensor.java
@@ -0,0 +1,22 @@
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.HiTechnicColorSensor;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+
+/**
+ *
+ */
+public class ColorSensor extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    
+    public HiTechnicColorSensor colorSensor = new HiTechnicColorSensor(RobotMap.colorSensor);
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
