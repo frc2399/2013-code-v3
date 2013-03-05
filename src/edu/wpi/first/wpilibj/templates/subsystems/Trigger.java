7e53ce35b0c067236f5bafdb9bdeac18461b1391
Jessie Adkins <zaheelee@gmail.com>
2013-03-04 20:20:58 -0500

    started work on switching the trigger from a servo to a window motor

--- src/edu/wpi/first/wpilibj/templates/subsystems/Trigger.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Trigger.java
@@ -2,28 +2,32 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.templates.RobotMap;
 import edu.wpi.first.wpilibj.templates.commands.Fire;
 import edu.wpi.first.wpilibj.HiTechnicColorSensor;
 
 /**
- * if things start breaking, check the RobotMap to make sure port #'s are correct!
  * @author Jessie
  */
 public class Trigger extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     
-    public Servo triggerMot = new Servo(RobotMap.triggerMot);
+    public CANJaguar triggerMot; 
     public HiTechnicColorSensor colorSensor = new HiTechnicColorSensor(RobotMap.colorSensor);
     
-    
+    public Trigger(){
+        try{
+            triggerMot = new CANJaguar(RobotMap.triggerMot);
+        }catch(Exception e){
+            
+        }
+    }
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-        setDefaultCommand(new Fire(0.4));
     }
     
     public void setSpeed(double speed){
