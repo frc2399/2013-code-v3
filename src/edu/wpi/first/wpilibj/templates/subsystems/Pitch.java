c672c363ef5f551b5f51de7c9257baa79ebb1911
Jessie Adkins <zaheelee@gmail.com>
2013-03-10 15:07:25 -0400

    fixed the PID for the Pitch using the values and things we got from the cool interface with the CAN Jags

--- src/edu/wpi/first/wpilibj/templates/subsystems/Pitch.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Pitch.java
@@ -1,7 +1,7 @@
 
 package edu.wpi.first.wpilibj.templates.subsystems;
 
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.CANJaguar.ControlMode;
 import edu.wpi.first.wpilibj.AnalogChannel;
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.templates.commands.ManPitch;
  *
  * @author Jessie
  */
-public class Pitch extends PIDSubsystem {
+public class Pitch extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     
@@ -19,16 +19,19 @@ public class Pitch extends PIDSubsystem {
     //this is different because it is a magnetic encoder
     public AnalogChannel pitchEncoder = new AnalogChannel(RobotMap.pitchEncoder);
     
-    final double MinAngle = 0;
-    //MAX ANGLE SHOULD BE 35, I AM JUST BEING CAREFUL!
-    final double MaxAngle = 30;
+    
     
     public Pitch(){
-        super(0.000001, 0, 0);
         
         try{
             //pitchMot = new CANJaguar(RobotMap.pitchMot);
             pitchMot = new CANJaguar(RobotMap.testPitchMot);
+            pitchMot.setPID(350, 0.02, 0.0);
+            pitchMot.changeControlMode(ControlMode.kPosition);
+            pitchMot.configSoftPositionLimits(2.5, 2.0);
+            pitchMot.setPositionReference(CANJaguar.PositionReference.kPotentiometer);
+            pitchMot.configNeutralMode(CANJaguar.NeutralMode.kCoast);
+            pitchMot.configPotentiometerTurns(10);
         }catch(Exception e){
             System.out.println(e);
         }
@@ -42,42 +45,18 @@ public class Pitch extends PIDSubsystem {
         //setDefaultCommand(new ManPitch());
     }
     
-    public void setSetpoint(double angle){
-        //this is to make sure the robot does not decapitatie itself
-        if (angle > MaxAngle) {
-            angle = MaxAngle;
-        } else if (angle < MinAngle) {
-            angle = MinAngle;
-        }
-        //this is the part that actually does something involving the controller
-        getPIDController().setSetpoint(angle);
-    }
     
-    protected double returnPIDInput(){
-        return pitchEncoder.pidGet();
-    }
-    
-    protected void usePIDOutput(double output){
-        //TEST THIS
-        //this is really small so that the robot does not decapitatie itself!
-        
-        try{
-            pitchMot.setX(output * 0.1);
-        }catch(Exception e){
-            System.out.println(e);
-        }
-          
-    }
     
     
     
     /**
-     * sets the speed of the pitch motor.  
-     * @param speed speed of the pitch motor. -1 <= speed <= 1.
+     * sets the position of the pitch motor.  
+     * @param position position of the pitch motor. 0 <= position <= 1. Zero is biggest angle, 1 is straight
      */
-    public void setSpeed(double speed){
+    public void setPostition(double position){
+        position = position * .5 + 2.0;
         try{
-            pitchMot.setX(speed);
+            pitchMot.setX(position);
         }catch(Exception e){
             System.out.println(e);
         }
